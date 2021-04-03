cd3843306af624fa334ef5b211eb5efa76f2f9cb
Maxsweel <maxsweelrodrigues@hotmail.com>
2021-04-03 14:12:42 -0300

    C-4 - Criamos o controle para listar na API

---
+++ WapiCatalogoEstudo/Controllers/ProdutosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WapiCatalogoEstudo.Context;
+using WapiCatalogoEstudo.Domains;
+
+namespace WapiCatalogoEstudo.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produtos>> Get()
+        {
+            return _context.Produtos.ToList();
+        }
+
+    }
+}
